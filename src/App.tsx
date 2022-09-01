d46478e57ec2f7f13fb010177de97771bc00489c
Marcos Iorio <marcossiorio@gmail.com>
2022-09-01 16:50:33 -0300

    added mobile support

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Grid from "@mui/material/Unstable_Grid2";
 import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 import FormContainer from "./components/form-container/FormContainer";
 import NotesContainer from "./components/notes-container/NotesContainer";
@@ -16,10 +17,17 @@ function App() {
     (state: RootState) => state.notification.notification
   );
 
+  const mobile = useMediaQuery("(max-width: 768px)");
+
+  console.log(mobile);
+
   const dispatch = useDispatch();
 
-  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
-    if (reason === 'clickaway') {
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
       return;
     }
     dispatch(
@@ -29,44 +37,94 @@ function App() {
     );
   };
 
-
-  return (
-    <Box sx={{ width: "100vw", height: "100%", backgroundColor: '#242424'}} >
-      {notification && (
-        <Notification
-          status={notification.status}
-          message={notification.message}
-          title={notification.title}
-          onClose={handleClose}
-          open={notification.open}
-        />
-      )}
-      <Grid
-        container
-        spacing={8}
-        columns={16}
-        sx={{ width: "100%", height: "100%", padding: "2em", backgroundColor: '#242424' }}
-      >
+  if (!mobile) {
+    return (
+      <Box sx={{ width: "100vw", height: "100%", backgroundColor: "#242424" }}>
+        {notification && (
+          <Notification
+            status={notification.status}
+            message={notification.message}
+            title={notification.title}
+            onClose={handleClose}
+            open={notification.open}
+          />
+        )}
         <Grid
-          xs={8}
+          container
+          spacing={8}
+          columns={16}
           sx={{
-            alignSelf: "start",
-            textAlign: "center",
-            padding: "5em",
-            position: "sticky",
-            top: "20px",
-            left: "0",
+            width: "100%",
             height: "100%",
+            padding: "2em",
+            backgroundColor: "#242424",
           }}
         >
-          <FormContainer />
+          <Grid
+            xs={8}
+            sx={{
+              alignSelf: "start",
+              textAlign: "center",
+              padding: "5em",
+              position: 'sticky',
+              top: "20px",
+              left: "0",
+              height: "100%",
+            }}
+          >
+            <FormContainer />
+          </Grid>
+          <Grid xs={8}>
+            <NotesContainer />
+          </Grid>
         </Grid>
-        <Grid xs={8}>
-          <NotesContainer />
+      </Box>
+    );
+  } else {
+    return (
+      <Box sx={{ width: "100vw", height: "100%", backgroundColor: "#242424" }}>
+        {notification && (
+          <Notification
+            status={notification.status}
+            message={notification.message}
+            title={notification.title}
+            onClose={handleClose}
+            open={notification.open}
+          />
+        )}
+        <Grid
+          container
+          direction="column"
+          sx={{
+            width: "100%",
+            height: "100%",
+            padding: "2em",
+            backgroundColor: "#242424",
+            alignItems: "center",
+          }}
+        >
+          <Grid
+            xs={8}
+            sx={{
+              alignSelf: "start",
+              textAlign: "center",
+              padding: "0",
+              position: "static",
+              top: "20px",
+              left: "0",
+              height: "100%",
+              width: '100%'
+            }}
+          >
+            <FormContainer />
+          </Grid>
+          <Grid xs={8} sx={{ width: "100%" }}>
+            <NotesContainer />
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-  );
+      </Box>
+    );
+  }
 }
 
 export default App;
--- src/components/form-container/FormContainer.tsx
+++ src/components/form-container/FormContainer.tsx
@@ -11,11 +11,11 @@ const FormContainer = () => {
     return(
         <Fragment>
           <h1>Start writing a Note!</h1>
-            <Card variant='outlined' className={styles.form__wrapper} sx={{height: '35em'}}>
+            <Card variant='outlined' className={styles.form__wrapper}>
                 <Form/>
             </Card>
             <div style={{marginTop: '50px'}}>
-                <p className={styles['github-title']}>See all my projects in my Github.</p>
+                <p className={styles['github-title']}>Check all my projects on Github.</p>
                 <a href="https://github.com/Marcos-Iorio" target="_blank"><GitHubIcon/></a>
             </div>
         </Fragment>
